73610b8ef847c1d5ee199a13005371a3614ab484
12jmanning <12jmanning@gmail.com>
2021-02-18 18:58:12 +0000

    up

--- confirmorder.php
+++ confirmorder.php
@@ -3,17 +3,26 @@
 session_start();
 if (isset($_SESSION['db_customerID'])) {
     include ("inc/detail.php");
+    include ("cart.php");
 
     $customer_ID = $_SESSION['db_customerID'];
     $delivery_date = $_POST['delivery_date'];
     $collection_date = $_POST['collection_date'];
     $delivery_and_collection = $_POST['delivery_and_collection'];
     $set_up = $_POST['set_up'];
+    $customer_cart = $_SESSION['cart'];
 
 
+    echo $subtotal;
+    echo $setup2;
+    echo $customer_ID;
+    echo $delivery_date;
+    echo $collection_date;
+    echo $delivery_and_collection;
+    echo $set_up;
+    echo $customer_cart;
+    echo '<pre>'; print_r($customer_cart); echo '</pre>';
 
-    echo $q;
-    header('location: confirmorder.php');
 }
 else{
     header('location: existingcustomers.php');
@@ -21,3 +30,14 @@ else{
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
