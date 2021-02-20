286fafe6d92ab6761d46b4d3e7d9077c3849f012
cartyad <cartyad@tcd.ie>
2021-02-20 22:50:03 +0000

    Update registerorder.php
    Reads into the product orders table successfully

--- registerorder.php
+++ registerorder.php
@@ -24,8 +24,15 @@ if (isset($_SESSION['db_customerID'])) {
     {
         $get_order_ID = ("SELECT orders.db_orderID FROM orders WHERE orders.db_customerID = $customer_ID AND db_deliveryDatetime = '$delivery_date'") ;
         $result = $db->query($get_order_ID);
-        $row = mysqli_fetch_assoc($result); 
-        $found_order_id=$row['db_orderID'];
+        $num_results = mysqli_num_rows($result);
+        $i=0;
+        $found_order_id="";
+        while($i<$num_results)
+        {
+            $row = mysqli_fetch_assoc($result); 
+            $found_order_id=$row['db_orderID'];
+            $i=$i+1;
+        }
         $order_ID=$found_order_id;
         $collection= "collection";
         $delivery= "delivery";
@@ -45,34 +52,56 @@ if (isset($_SESSION['db_customerID'])) {
         $_SESSION['db_orderID']= $order_ID;
     }
 
+    function pdo_connect_mysql() {
+        // Update the details below with your MySQL details
+        $DATABASE_HOST = 'localhost';
+        $DATABASE_USER = 'group_6';
+        $DATABASE_PASS = 'zu5ahLie';
+        $DATABASE_NAME = 'stu33001_2021_group_6_db';
+        try {
+            return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+        } catch (PDOException $exception) {
+            // If there is an error with the connection, stop the script and display the error.
+            exit('Failed to connect to database!');
+        }
+    }
     //Here the product order table is going to be populated 
-
+    $pdo = pdo_connect_mysql();
     $products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
     $products = array();
-    $subtotal = 0.00;
     // If there are products in cart
     if ($products_in_cart) {
         // There are products in the cart so we need to select those products from the database
         // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
         $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
+        #ERROR IN THIS NEXT LINE
         $stmt = $pdo->prepare('SELECT * FROM products WHERE db_productID IN (' . $array_to_question_marks . ')');
         // We only need the array keys, not the values, the keys are the id's of the products
         $stmt->execute(array_keys($products_in_cart));
         // Fetch the products from the database and return the result as an Array
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // Calculate the subtotal
+        
         foreach ($products as $product) {
-            $product_order_product_ID = $product['db_productID'];
-            $product_order_product_quantity= $products_in_cart[$product['db_productID']];
-            $orderID_product_table = $_SESSION['db_orderID'];
-            $product_order_query = "INSERT INTO transit (";
+
+            $product_order_product_ID = (int)$product['db_productID'];
+            $product_order_product_quantity= (int)$products_in_cart[$product['db_productID']];
+            $orderID_product_table = $_SESSION['db_orderID']; 
+
+            /*$product_order_product_ID = 1;
+            $product_order_product_quantity= 1;
+            $orderID_product_table = 1;*/
+
+            $product_order_query = "INSERT INTO product_orders (";
             $product_order_query .= "db_orderID, db_productID, db_quantityOrdered";
             $product_order_query .= ") VALUES (";
             $product_order_query .= "'$orderID_product_table', '$product_order_product_ID', '$product_order_product_quantity')";
+
             $result_collection = $db->query($product_order_query);
         }
 
     }
+
+    header('location: index.php');
 }
 else{
     header('location: existingcustomers.php');
