23022b8a75116c91074d4145cb6d7bf543328dad
cartyad <cartyad@tcd.ie>
2021-02-21 13:59:36 +0000

    Update admindashboard.php

--- admindashboard.php
+++ admindashboard.php
@@ -1,4 +1,15 @@
+
 <?PHP
+// 1) Validation of inventory control in relation to the dates and quatities available
+// 2) County Dropdown list in create new customers 
+// 3) Clock In system linked to employee table
+// 4) Damage report 
+// 5) Dashboard Customer, Employee and Admin
+// 6) Consistent design
+// 7) All commenting
+
 include('inc/detail.php');
 include('inc/navbar.php');
+
+
 ?>
