41ab38276d030086b17304ff889d2379a645c604
jack <manninjat@tcd.ie>
2021-02-21 14:05:08 +0000

    accidental

--- admindashboard.php
+++ admindashboard.php
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-<?PHP
-include('inc/detail.php');
-include('inc/navbar.php');
-=======
 
 <?PHP
 // 1) Validation of inventory control in relation to the dates and quatities available
-// 2) County Dropdown list in create new customers 
+// 2) County Dropdown list in create new customers
 // 3) Clock In system linked to employee table
-// 4) Damage report 
+// 4) Damage report
 // 5) Dashboard Customer, Employee and Admin
 // 6) Consistent design
 // 7) All commenting
@@ -17,5 +12,4 @@ include('inc/detail.php');
 include('inc/navbar.php');
 
 
->>>>>>> 23022b8a75116c91074d4145cb6d7bf543328dad
 ?>
--- registerorder.php
+++ registerorder.php
@@ -29,7 +29,7 @@ if (isset($_SESSION['db_customerID'])) {
         $found_order_id="";
         while($i<$num_results)
         {
-            $row = mysqli_fetch_assoc($result); 
+            $row = mysqli_fetch_assoc($result);
             $found_order_id=$row['db_orderID'];
             $i=$i+1;
         }
@@ -42,7 +42,7 @@ if (isset($_SESSION['db_customerID'])) {
         $q2 .= ") VALUES (";
         $q2 .= "'$order_ID', '$collection')";
         $result_collection = $db->query($q2);
-        
+
         $q1  = "INSERT INTO transit (";
         $q1 .= "db_orderID, db_transitType";
         $q1 .= ") VALUES (";
@@ -57,7 +57,6 @@ if (isset($_SESSION['db_customerID'])) {
         $transit_record2 = "UPDATE orders, transit SET orders.db_collectionID = transit.db_transitID WHERE orders.db_orderID = '$order_ID' AND orders.db_orderID = transit.db_orderID AND transit.db_transitType = '$collection'";
         $update_order2 = $db->query($transit_record2);
 
-<<<<<<< HEAD
     }
 
     function pdo_connect_mysql() {
@@ -73,7 +72,7 @@ if (isset($_SESSION['db_customerID'])) {
             exit('Failed to connect to database!');
         }
     }
-    //Here the product order table is going to be populated 
+    //Here the product order table is going to be populated
     $pdo = pdo_connect_mysql();
     $products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
     $products = array();
@@ -88,64 +87,12 @@ if (isset($_SESSION['db_customerID'])) {
         $stmt->execute(array_keys($products_in_cart));
         // Fetch the products from the database and return the result as an Array
         $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
+
         foreach ($products as $product) {
 
             $product_order_product_ID = (int)$product['db_productID'];
             $product_order_product_quantity= (int)$products_in_cart[$product['db_productID']];
-            $orderID_product_table = $_SESSION['db_orderID']; 
-
-            /*$product_order_product_ID = 1;
-            $product_order_product_quantity= 1;
-            $orderID_product_table = 1;*/
-
-            $product_order_query = "INSERT INTO product_orders (";
-            $product_order_query .= "db_orderID, db_productID, db_quantityOrdered";
-            $product_order_query .= ") VALUES (";
-            $product_order_query .= "'$orderID_product_table', '$product_order_product_ID', '$product_order_product_quantity')";
-
-            $result_collection = $db->query($product_order_query);
-        }
-
-    }
-
-=======
-    }
-
-    function pdo_connect_mysql() {
-        // Update the details below with your MySQL details
-        $DATABASE_HOST = 'localhost';
-        $DATABASE_USER = 'group_6';
-        $DATABASE_PASS = 'zu5ahLie';
-        $DATABASE_NAME = 'stu33001_2021_group_6_db';
-        try {
-            return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-        } catch (PDOException $exception) {
-            // If there is an error with the connection, stop the script and display the error.
-            exit('Failed to connect to database!');
-        }
-    }
-    //Here the product order table is going to be populated 
-    $pdo = pdo_connect_mysql();
-    $products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
-    $products = array();
-    // If there are products in cart
-    if ($products_in_cart) {
-        // There are products in the cart so we need to select those products from the database
-        // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
-        $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
-        #ERROR IN THIS NEXT LINE
-        $stmt = $pdo->prepare('SELECT * FROM products WHERE db_productID IN (' . $array_to_question_marks . ')');
-        // We only need the array keys, not the values, the keys are the id's of the products
-        $stmt->execute(array_keys($products_in_cart));
-        // Fetch the products from the database and return the result as an Array
-        $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        foreach ($products as $product) {
-
-            $product_order_product_ID = (int)$product['db_productID'];
-            $product_order_product_quantity= (int)$products_in_cart[$product['db_productID']];
-            $orderID_product_table = $_SESSION['db_orderID']; 
+            $orderID_product_table = $_SESSION['db_orderID'];
 
             /*$product_order_product_ID = 1;
             $product_order_product_quantity= 1;
@@ -162,7 +109,6 @@ if (isset($_SESSION['db_customerID'])) {
     }
 //Change the location of page to an invoice
 //Session variables are not deleted here
->>>>>>> 23022b8a75116c91074d4145cb6d7bf543328dad
     header('location: index.php');
 }
 else{
