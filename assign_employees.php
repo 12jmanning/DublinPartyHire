8c5b1441d012ecbb833dd89e0cc50942b0b2792d
cartyad <cartyad@tcd.ie>
2021-02-22 13:05:33 +0000

    Update assign_employees.php

--- assign_employees.php
+++ assign_employees.php
@@ -8,8 +8,6 @@ $employee_ID = $_SESSION['db_employeeID'];
 $employee_name = $_SESSION['db_employeeName'];
 $job_title = $_SESSION['db_jobTitle'];
 $yes = "Yes";
+$order_ID = $_POST['order_id'];
 
-$query1 = "select orders.db_orderID, transit.db_transitType from orders,transit where db_deliveryPreference = '$yes' AND ((orders.db_deliveryDatetime= '$today_date' AND orders.db_deliveryID = transit.db_transitID ) OR (orders.db_deliveryDatetime= '$today_date' AND orders.db_collectionID = transit.db_transitID))";
-$orders_today = $db->query($query1);
-$num_results = mysqli_num_rows($orders_today);
 ?>
