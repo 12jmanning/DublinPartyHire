dc178b1f500cb5adc2c3b92b09595b6a890d76ca
miocalla <61199986+miocalla@users.noreply.github.com>
2021-03-04 18:25:22 +0000

    Rental Frequency Query

--- reports.php
+++ reports.php
@@ -15,3 +15,35 @@ include('inc/navbar.php');
 
 
  ?>
+
+<h2 class="auto-style1">Rental Frequency</h2>
+<p>List of products and the number of times they have been rented.</p>
+
+<table id="table">
+	<tr>
+		<th class="auto-style1"><strong>Product Name</strong></th>
+		<th class="auto-style1"><strong>Rental Frequency</strong></th>
+	</tr>
+	<?php
+
+	include("detail.php");
+	$sql = "SELECT db_productName AS 'Product', count(*) AS 'Count'
+	FROM product_orders, products
+	WHERE product_orders.db_productID = products.db_productID
+	GROUP BY db_productName
+	ORDER BY products.db_productID"; 
+		$result = $db->query($sql);
+	
+		$num_results = mysqli_num_rows($result);
+		for($i=0; $i < $num_results; $i++)
+		{
+			$row = mysqli_fetch_assoc($result);
+			echo "<tr>";
+		                   echo "<td>".$row[Product]."</td>";
+		                   echo "<td>".$row[Count]."</td>";
+		                   echo "</tr>";
+		}
+		mysqli_close($db);
+?>	
+
+</table>
