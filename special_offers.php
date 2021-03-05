f603452c498449a04f1564c3d564127189683414
cartyad <cartyad@tcd.ie>
2021-03-05 01:04:56 +0000

    Update special_offers.php
    Working Special Offers with validations

--- special_offers.php
+++ special_offers.php
@@ -20,21 +20,56 @@ if($job_title==$admin){
 $productErr = $product1Err =$product2Err= $product3Err= "";
 $valid=true;
 if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
-    if (empty($_POST["product_id"])) {
+    if ($_POST["product_id"]=="select") {
         $productErr = "Product is required";
         $valid=false;
     }
-    if (empty($_POST["product_id1"])) {
+    if ($_POST["product_id1"]=="select") {
         $product1Err = "Product is required";
         $valid=false;
     }
-    if (empty($_POST["product_id2"])) {
+    if ($_POST["product_id2"]=="select") {
         $product2Err = "Product is required";
         $valid=false;
     }
-    if (empty($_POST["product_id3"])) {
+    if ($_POST["product_id3"]=="select") {
         $product3Err = "Product is required";
         $valid=false;
+    }
+    if($valid)
+    {
+        if($_POST["product_id"]==$_POST["product_id1"]||$_POST["product_id"]==$_POST["product_id2"]||$_POST["product_id"]==$_POST["product_id3"])
+        {
+            $productErr = "Must Select Different Products";
+            $product1Err = "Must Select Different Products";
+            $product2Err = "Must Select Different Products";
+            $product3Err = "Must Select Different Products";
+            $valid=false;
+        }
+        else if($_POST["product_id1"]==$_POST["product_id"]||$_POST["product_id1"]==$_POST["product_id2"]||$_POST["product_id1"]==$_POST["product_id3"])
+        {
+            $productErr = "Must Select Different Products";
+            $product1Err = "Must Select Different Products";
+            $product2Err = "Must Select Different Products";
+            $product3Err = "Must Select Different Products";
+            $valid=false;
+        }
+        else if($_POST["product_id2"]==$_POST["product_id"]||$_POST["product_id2"]==$_POST["product_id1"]||$_POST["product_id2"]==$_POST["product_id3"])
+        {
+            $productErr = "Must Select Different Products";
+            $product1Err = "Must Select Different Products";
+            $product2Err = "Must Select Different Products";
+            $product3Err = "Must Select Different Products";
+            $valid=false;
+        }
+        else if($_POST["product_id3"]==$_POST["product_id"]||$_POST["product_id3"]==$_POST["product_id1"]||$_POST["product_id3"]==$_POST["product_id2"])
+        {
+            $productErr = "Must Select Different Products";
+            $product1Err = "Must Select Different Products";
+            $product2Err = "Must Select Different Products";
+            $product3Err = "Must Select Different Products";
+            $valid=false;
+        }
     }
 
 
@@ -43,9 +78,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
     if($valid!=false)
     {
         $db_productID =$_POST['product_id'];
+        $db_productID1 =$_POST['product_id1'];
+        $db_productID2 =$_POST['product_id2'];
+        $db_productID3 =$_POST['product_id3'];
 
-        $query1 = "UPDATE products SET db_quantity = '$db_quantity' WHERE db_productID = '$db_productID'";
+        $query1 = "UPDATE special_offers SET db_productID = '$db_productID' WHERE db_specialOfferID = 1";
         $edit_product1 = $db->query($query1);
+
+        $query2 = "UPDATE special_offers SET db_productID = '$db_productID1' WHERE db_specialOfferID = 2";
+        $edit_product2 = $db->query($query2);
+
+        $query3 = "UPDATE special_offers SET db_productID = '$db_productID2' WHERE db_specialOfferID = 3";
+        $edit_product3 = $db->query($query3);
+
+        $query4 = "UPDATE special_offers SET db_productID = '$db_productID3' WHERE db_specialOfferID = 4";
+        $edit_product4 = $db->query($query4);
+        header('Location: admindashboard.php');
+
     }
 
 }
@@ -53,7 +102,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
 
 ?>
 
-/*<div class="row" style="padding-top:25px;">
+<div class="row" style="padding-top:25px;">
 
   <div class="col-lg-1">
 
@@ -76,6 +125,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
     <tr>
       <td><label for="order_id">Product 1:</label></td>
       <td style="width: 399px; height: 38px;" class="auto-style2">
+      <span class='error'> <?php echo $productErr ?> <span>
       <select name="product_id" style="width: 399px" class="auto-style1" required>
       <option value= "select">--Select a Product--</option>
       <?php
@@ -91,6 +141,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
     <tr>
       <td><label for="order_id">Product 2:</label></td>
       <td style="width: 399px; height: 38px;" class="auto-style2">
+      <span class='error'> <?php echo $product1Err ?> <span>
       <select name="product_id1" style="width: 399px" class="auto-style1" required>
       <option value= "select">--Select a Product--</option>
       <?php
@@ -106,6 +157,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
     <tr>
       <td><label for="order_id">Product 3:</label></td>
       <td style="width: 399px; height: 38px;" class="auto-style2">
+      <span class='error'> <?php echo $product2Err ?> <span>
       <select name="product_id2" style="width: 399px" class="auto-style1" required>
       <option value= "select">--Select a Product--</option>
       <?php
@@ -121,6 +173,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['submit2'])) {
     <tr>
       <td><label for="order_id">Product 4:</label></td>
       <td style="width: 399px; height: 38px;" class="auto-style2">
+      <span class='error'> <?php echo $product3Err ?> <span>
       <select name="product_id3" style="width: 399px" class="auto-style1" required>
       <option value= "select">--Select a Product--</option>
       <?php
