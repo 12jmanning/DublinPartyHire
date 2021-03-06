a5c36c40ca00ac3fe54b25548d3cc08a17bd3fa4
miocalla <61199986+miocalla@users.noreply.github.com>
2021-03-06 15:31:09 +0000

    Update reports.php
    Added Sales Revenue by product table

--- reports.php
+++ reports.php
@@ -4,8 +4,8 @@ include('inc/navbar.php');
 
 // delivery pick / up schedule by date
 // order check - JACK
-// rental frequency
-// sales revenue
+// rental frequency DONE
+// sales revenue DONE
 // top grossing rentals
 // top customers
 // three other reports
@@ -26,7 +26,6 @@ include('inc/navbar.php');
 	</tr>
 	<?php
 
-	include("detail.php");
 	$sql = "SELECT db_productName AS 'Product', count(*) AS 'Count'
 	FROM product_orders, products
 	WHERE product_orders.db_productID = products.db_productID
@@ -48,6 +47,36 @@ include('inc/navbar.php');
 
 </table>
 
+<h2 class="auto-style1">Sales Revenue by Product</h2>
+<p>Breakdown of the revenue earned from each rental product</p>
+
+<table id="table">
+	<tr>
+		<th class="auto-style1"><strong>Product Name</strong></th>
+		<th class="auto-style1"><strong>Revenue</strong></th>
+	</tr>
+	<?php
+	$sql = "SELECT db_productName AS 'Product', db_productPrice * sum(db_quantityOrdered) * count(*) AS 'Revenue'
+	FROM product_orders, products
+	WHERE product_orders.db_productID = products.db_productID
+	GROUP BY db_productName
+	ORDER BY products.db_productID"; 
+		$result = $db->query($sql);
+	
+		$num_results = mysqli_num_rows($result);
+		for($i=0; $i < $num_results; $i++)
+		{
+			$row = mysqli_fetch_assoc($result);
+			echo "<tr>";
+		                   echo "<td>".$row[Product]."</td>";
+		                   echo "<td>".$row[Revenue]."</td>";
+		                   echo "</tr>";
+		}
+		mysqli_close($db);
+?>	
+
+</table>
+
 <h2 class="auto-style1">Extra Reports</h2>
 
 <h2 class="auto-style1">Most Ordered Products by Quantity</h2>
@@ -60,7 +89,6 @@ include('inc/navbar.php');
 	</tr>
 	<?php
 
-	include("detail.php");
 	$sql = "SELECT db_productName, sum(db_quantityOrdered) AS 'Quantity'
 	FROM product_orders, products
 	WHERE product_orders.db_productID = products.db_productID
@@ -73,8 +101,8 @@ include('inc/navbar.php');
 		{
 			$row = mysqli_fetch_assoc($result);
 			echo "<tr>";
-		                   echo "<td>".$row[db_productName]."</td>";
-		                   echo "<td>".$row[Quantity]."</td>";
+		                   echo "<td>".$row['db_productName']."</td>";
+		                   echo "<td>".$row['sum(db_quantityOrdered)']."</td>";
 		                   echo "</tr>";
 		}
 		mysqli_close($db);
