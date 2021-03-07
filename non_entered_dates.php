b5a53a1848d7bf8933643ac0eff8b610768001a0
jack <manninjat@tcd.ie>
2021-03-07 18:06:40 +0000

    changes to breakages and non_entered dates

--- non_entered_dates.php
+++ non_entered_dates.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['update'])) {
     $grand=false;
   }
   else if (empty($_POST['collection_date'])) {
-    $deliveryDateErr = "Collection Date is required";
+    $collectionDateErr = "Collection Date is required";
     $grand=false;
   }
   else if($_POST['delivery_date']>$_POST['collection_date'])
@@ -41,11 +41,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['update'])) {
     $deliveryDateErr =  "Please Enter Valid Future Dates";
     $grand=false;
   }*/
-  else if (isValidDate($_POST['delivery_date'],'Y-m-d')) 
+  else if (isValidDate($_POST['delivery_date'],'Y-m-d'))
   {
     $deliveryDateErr =  "Please Enter Valid Future Dates";
-    $grand=false;    
-  }  
+    $grand=false;
+  }
   if($grand==true)
   {
 
@@ -62,40 +62,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"&& isset($_POST['update'])) {
 ?>
 
 
-<div class="row" style="padding-top:25px;">
+<div class="row" style="padding-top:10%;">
 
-  <div class="col-lg-1">
+  <div class="col-lg-2">
 
   </div>
-  <div class="col-lg-4">
-    <h2>Details:</h2>
+  <div class="col-lg-8" style="background-color: #e8e8e8;">
+    <h2 style="text-align: -webkit-center; margin-top:20px;">Enter Delivery and Collection Dates:</h2>
     <?php
         if(isset($_SESSION['delivery_date'])&&isset($_SESSION['collection_date'])){
             echo "Order Start Date: ", $_SESSION['delivery_date'], "<br>";
             echo "Order End Date: ", $_SESSION['collection_date'], "<br>";
         }
         else{
-            echo "Please enter valid dates for your order before proceeding to the products page.<br>This will allow you to see the products and quantities available for your event!";
+            echo "<p style='text-align: -webkit-center;'>
+            Please enter valid dates for your order before proceeding to the products page.<br>This will allow you to see the products and quantities available for your event!
+            </p>";
         }
      ?>
+
+     <form method="post" action = ""  name="order_form" id="order_form" style="text-align: -webkit-center;">
+       <br><br>
+       <table>
+
+
+         <tr>
+           <td><label for="delivery_date" style="padding-right: 20px;">Delivery Date:</label></td>
+           <td><input type="date" name="delivery_date" id="delivery_date" style="width: 150px;"></td>
+             <td><label for="collection_date" style=" padding-left: 20px;">Collection Date:</label></td>
+             <td><input type="date" name="collection_date" id="collection_date" style="width:150px;" ></td>
+             <td><input class="btn" type="submit" value="Filter Products" name="update" style="background-color: #C46BAE; color: #fff; margin:auto; margin-left: 20px;"></td>
+         </tr>
+
+         <tr>
+           <td></td>
+           <td><?php echo $deliveryDateErr ?></td>
+           <td></td>
+           <td><?php echo $collectionDateErr ?></td>
+           <td></td>
+         </tr>
+
+        </table>
+        <p style="text-align: -webkit-center;">Return To Homepage <a href="index.php">HERE</a></p>
+       </form>
   </div>
 
 
-  <div class="col-lg-6" >
-  <form method="post" action = ""  name="order_form" id="order_form" style="text-align: -webkit-center;">
-    <br><br>
-      <tr>
-        <td><label for="delivery_date" style="padding-right: 20px;">Delivery Date:</label></td>
-        <td ><input type="date" name="delivery_date" id="delivery_date" style="width: 150px;"><?php echo $deliveryDateErr ?> <span></td>
-          <td><label for="collection_date" style="padding-right: 20px; padding-left: 20px;">Collection Date:</label></td>
-          <td><input type="date" name="collection_date" id="collection_date" style="width:150px;" ><?php echo $collectionDateErr ?> <span></td>
-          <td><input class="btn" type="submit" value="Filter Products" name="update" style="background-color: #C46BAE; color: #fff; margin:auto; margin-left: 20px;"></td>
-      </tr>
-      <tr>
-      <td></td>
-      <td><p>Return To Homepage <a href="index.php">HERE</a></p></td>
-    </tr>
-    </form>
+  <div class="col-lg-2" >
   </div>
 
 </div>
