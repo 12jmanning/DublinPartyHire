2e4dc417bd68abb6ab681e87e5838b8b822397c0
cartyad <cartyad@tcd.ie>
2021-03-10 01:25:18 +0000

    Log-Out

--- employeedashboard.php
+++ employeedashboard.php
@@ -523,7 +523,7 @@ function reload3(form)
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="/group_6/">Logout</a>
+                    <a class="btn btn-primary" href="log-out.php">Logout</a>
                 </div>
             </div>
         </div>
--- log-out.php
+++ log-out.php
@@ -6,6 +6,8 @@ unset($_SESSION['db_customerID']);
 unset($_SESSION['cart']);
 unset($_SESSION['link']);
 unset($_SESSION['db_jobTitle']);
+unset($_SESSION['db_employeeName']);
+unset($_SESSION['db_employeeID']);
 session_destroy();
 header("Location: index.php");
 
